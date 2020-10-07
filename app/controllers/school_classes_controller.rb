7ddd1b1117765f61c1789fb1949c52040e0c4e20
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-10-06 21:59:47 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,27 @@
+class SchoolClassesController < ApplicationController
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+    
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+    private
+
+    def school_class_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+
+
+end
